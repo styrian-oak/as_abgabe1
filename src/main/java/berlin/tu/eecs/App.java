d3dc5419c3f43b389af88963aede916f5f32be8a
styrian-oak <242596607+styrian-oak@users.noreply.github.com>
2026-05-24 19:57:45 +0200

    Füge Methode hinzu, die Pfad nimmt und Inhalt der Datei als String zurückgibt
    Und rufe diese in der main-Methode auf

--- src/main/java/berlin/tu/eecs/App.java
+++ src/main/java/berlin/tu/eecs/App.java
@@ -1,10 +1,9 @@
 package berlin.tu.eecs;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String bookPath = "buecher/frankenstein.txt";
+        String text = BookReader.getBookText(bookPath);
+        System.out.println(text);
     }
 }
