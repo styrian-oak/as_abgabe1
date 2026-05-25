a9908ef4a423a9ce8b0b08bd3170d629be23beb2
styrian-oak <242596607+styrian-oak@users.noreply.github.com>
2026-05-25 12:41:00 +0200

    Füge Methode hinzu, die .txt liest & als Liste speichert
    Inhalt (Liste) wird in TxtReader-Klasse gespeichert

--- src/main/java/berlin/tu/eecs/TxtReader.java
+++ src/main/java/berlin/tu/eecs/TxtReader.java
@@ -1,6 +1,5 @@
 package berlin.tu.eecs;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -10,9 +9,31 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-@AllArgsConstructor
 public class TxtReader {
     private @Getter String filePath;
+    private List<String> lines;
 
-    //public static List<String> getTxtList(String filePath) {}
+    // Kein Lombok-Konstruktor mögl., weil wir Pfad für getTxtList-Aufruf
+    // brauchen
+    public TxtReader(String filePath) {
+        this.filePath = filePath;
+
+        // Speichere Inhalt zur weiteren Verarbeitung
+        this.lines = getTxtList(filePath);
+    }
+
+    // Lese Inhalt Zeile für Zeile ein (Stream wird autom. geschlossen)
+    public static List<String> getTxtList(String path) {
+        List<String> content = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                content.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
 }
