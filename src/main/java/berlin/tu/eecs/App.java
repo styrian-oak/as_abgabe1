b93468dd5a04f77a1e021961dd6fc3dfe5649b90
styrian-oak <242596607+styrian-oak@users.noreply.github.com>
2026-05-25 12:54:19 +0200

    Test: Gebe eine Datei auf Konsole aus

--- src/main/java/berlin/tu/eecs/App.java
+++ src/main/java/berlin/tu/eecs/App.java
@@ -4,5 +4,8 @@ public class App {
     public static void main(String[] args) {
         TxtReader file = new TxtReader("buecher/frankenstein.txt");
         System.out.println(file.getFilePath());
+        for (String line : file.getLines()) {
+            System.out.println(line);
+        }
     }
 }
--- src/main/java/berlin/tu/eecs/TxtReader.java
+++ src/main/java/berlin/tu/eecs/TxtReader.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class TxtReader {
     private @Getter String filePath;
-    private List<String> lines;
+    private @Getter List<String> lines;
 
     // Kein Lombok-Konstruktor mögl., weil wir Pfad für getTxtList-Aufruf
     // brauchen
